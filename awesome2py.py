6fc13dd6b61913cac510e2b0528cc05964d513e0
kikass13 <nickfiege999@gmail.com>
2021-10-06 11:23:09 +0200

    added sublisting feature [HACKED AF] for the awesoem list parser, see https://github.com/kikass13/awesome2py/issues/1

--- awesome2py.py
+++ awesome2py.py
@@ -29,9 +29,9 @@ class AwesomeListEntry(object):
         me, children = entry
 
         self.depth = depth
-        htmldata = me.find("a", href=True).extract()
-        self.url = htmldata["href"].strip()
-        self.name = htmldata.get_text().strip()
+        self.htmldata = me.find("a", href=True).extract()
+        self.url = self.htmldata["href"].strip()
+        self.name = self.htmldata.get_text().strip()
         self.text = me.get_text().strip()
 
         self.children = []
@@ -53,38 +53,64 @@ class AwesomeList(object):
         soup = self.convertFromHtml(path)
         contents, d = self.generateDict(soup)
         self.createStructure(contents, d)
-
     def convertFromHtml(self, path):
         html = markdown(open(path).read())
         soup = BeautifulSoup(html, features='html.parser')
         #print(soup.prettify())
         return soup
-    
+    def findOutIfSubListsAreUsed(self, soup):
+        toc = soup.find("h3")
+        return toc != None
+
+    def findContents(self, soup):
+        d = {}
+        ### find content h2
+        while True:
+            toc = soup.find("h2")
+            if toc and toc.get_text() == "Contents":
+                toc.extract() ## extract will consume the item
+                aList = self.findList(soup)
+                if aList:
+                    for item in self.findListItems(aList):
+                        ali = AwesomeListEntry(item)
+                        d[ali.name] = ali.htmldata
+                break
+        ### remove "Contents" from contents dict
+        if "Contens" in d:
+            del d["Contents"]
+        return d
+
     def generateDict(self, soup):
+        ### we ll specifically fetch the contents entry        
+        contents = self.findContents(soup)
+        ### find out if sub list entries were used here [pre consume] ... (experimental)
+        subListsAreUsed = self.findOutIfSubListsAreUsed(soup)
         ### crawl though all categories and extract information
-        d = self.findLists(soup)
         ### ...
-        ### well specifically fetch the contents entry
-        contentKey = "Contents"
-        contents = d[contentKey]
-        if contentKey in d:
-            del d[contentKey]
+        ### if sublists are used, we have now parsed the h2 main list entries
+        ### as well as the h3 sub list entries ... which results in every entry being 
+        d = self.findLists(soup, subListsAreUsed)
         return contents, d
 
     def createStructure(self, contents, d):
-        children = self.findListItems(contents, ignoreSubLists=True) 
-        for c in children:
-            rubricKey = c.get_text()
+        #children = self.findListItems(contents, ignoreSubLists=True) 
+        for c,v in contents.items():
+            rubricKey = c
             rubricEntries = d.get(rubricKey, None)
             if rubricEntries:
                 entries = self.findListItems(rubricEntries)
                 self.rubrics.append(AwesomeListRubric(rubricKey, entries))
 
     ########################################################
-    def findLists(self, soup):
+    def findLists(self, soup, subListsAreUsed = False):
         d = {}
         while True:
-            toc = soup.find("h2")
+            ### if there is no major content list defind anymore, we try parsing sub lists
+            if subListsAreUsed:
+                toc = soup.find("h3")
+            else:
+                toc = soup.find("h2")
+            ### start analyzing whatever list was found
             if toc:
                 toc.extract() ## extract will consume the item
                 aList = self.findList(soup)
@@ -127,7 +153,6 @@ class AwesomeList(object):
         return str(self)
 
 
-
 def main():
     path = "samples/awesomeListSample.md"
     if(len(sys.argv) > 1):
@@ -135,15 +160,19 @@ def main():
 
     alc = AwesomeList(path)
     print("===============================================")
-    
-    print(alc)
-    #for r in alc.rubrics:
-    #    for e in r.entries:
-    #        ### e.name
-    #        ### e.url
-    #        ### e.text
-    #        print(e)
-
+    #print(alc)
+    total = 0
+    for r in alc.rubrics:
+        print("%s [%s]" % (r.key, len(r.entries)))
+        total += len(r.entries)
+        for e in r.entries:
+           ### e.name
+           ### e.url
+           ### e.text
+           #print(e)
+           pass
+    print("===============================================")
+    print("Done parsing '%s' entries." % total)
 
 if __name__ == "__main__":
     main()
